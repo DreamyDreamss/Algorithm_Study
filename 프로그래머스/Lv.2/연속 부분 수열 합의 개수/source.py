9478b6df623b8e24ca86f6d8fa2b3c7eb1627765
Leejoohyeong <cheng1230@nate.com>
2022-12-20 00:34:05 +0900

    programers lv2 upload(22/12/20)

--- 프로그래머스/Lv.2/연속 부분 수열 합의 개수/source.py
+++ 프로그래머스/Lv.2/연속 부분 수열 합의 개수/source.py
@@ -1,8 +1,16 @@
 def solution(elements):
     answer = 0
+    ans_set = set()
 
+    for i in range(1,len(elements)):
+        for idx in range(0,len(elements)):
+            t = elements[idx:idx+i]
+            if idx+i > len(elements):
+                ed = idx+i - len(elements)
+                t = t+ elements[0:ed]
+            ans_set.add(sum(t))
+    ans_set.add(sum(elements))
+    return len(ans_set)
 
-    return answer
-
-elements = [7,9,1,1,4]
+elements = [0,1,2,3,4]
 solution(elements)
