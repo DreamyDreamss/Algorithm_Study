d6a968732edd168e30727ffe070d60c51b91e57c
Leejoohyeong <cheng1230@nate.com>
2022-12-27 15:58:22 +0900

    할인행사 solution 수정 (22/12/27)

--- 프로그래머스/Lv.2/할인 행사/source.py
+++ 프로그래머스/Lv.2/할인 행사/source.py
@@ -6,10 +6,18 @@ def solution(want, number, discount):
     for i in range(0,len(discount)-9):
         temp = Counter(discount[i:i+10])
 
+        flag = True
+        for idx,value in enumerate(want):
+            if temp[value] < number[idx]:
+                flag = False
         
+        if flag:
+            answer+=1
+    print(answer)
     return answer
-
 
 want = ["banana", "apple", "rice", "pork", "pot"]
 number = [3, 2, 2, 2, 1]
 discount = ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"]
+
+solution(want,number,discount)
