58bce8daeead5bb128353d3020dbf620333ca07f
Leejoohyeong <cheng1230@nate.com>
2022-12-29 23:29:53 +0900

    프로그래머스 lv2 업로드 (22/12/29)

---
+++ 프로그래머스/Lv.2/n^2 배열자르기/source.py
@@ -0,0 +1,13 @@
+def solution(n, left, right):
+    answer = []
+
+    arr = [[0 for j in range(n)] for i in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            arr[i] 
+
+    return answer
+
+
+solution(3,1,1)
