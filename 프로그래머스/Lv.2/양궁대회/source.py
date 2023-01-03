8e75ff49254d25c6e41f6fd92833d9816a1fe8fd
Leejoohyeong <cheng1230@nate.com>
2023-01-03 22:44:32 +0900

    양궁대회 RETRY (23/1/3)

--- 프로그래머스/Lv.2/양궁대회/source.py
+++ 프로그래머스/Lv.2/양궁대회/source.py
@@ -1,40 +1,54 @@
 import copy
+MAX_LIST = []
+MAX_SCORE = -100
 
 def compare(info,comp):
-    tot = 0
-    for i in range(0,11):
-        if info[i] >= comp[i]:
-            tot -= (10-i)
+    enemy_score = 0
+    my_score = 0  
+    for i in range(11):
+        if (info[i]==0 and comp[i]==0): 
+            pass
         else:
-            tot += (10-i)
-    
-    print(tot)
+            if info[i] >= comp[i]:
+                enemy_score += (10-i)
+            elif info[i] < comp[i]:
+                my_score += (10-i)
+    #print(comp,"###", my_score - enemy_score,my_score,enemy_score)    
 
-    return tot
+    return my_score - enemy_score
 
 def dfs(idx,lim,info,comp):
-        #print(f"idx: {idx},lim : {lim},comp : {comp}")
-        if idx == 11 or lim<=0:
-            print(comp)
-            return 0
-            #return compare(info,comp)
-        tt = copy.deepcopy(comp)
+    global MAX_LIST, MAX_SCORE
 
-        dfs(idx+1,lim,info,tt)
+    if idx == 11:
+        if lim!=0:
+            comp[10] = lim
+        comp_score = compare(info,comp)
 
-        if lim >= info[idx]+1:
-            
-            tt[idx] = info[idx]+1
-            dfs(idx+1,lim-tt[idx],info,tt)
+        if MAX_SCORE < comp_score:
+            MAX_LIST.clear()
+            MAX_SCORE = comp_score
+            MAX_LIST.append(comp)
+
+        elif MAX_SCORE == comp_score:
+            MAX_LIST.append(comp)
+        return 0
+
+    tt = copy.deepcopy(comp)
+    dfs(idx+1,lim,info,tt)
+    if lim >= info[idx]+1:
+        tt[idx] = info[idx]+1
+        dfs(idx+1,lim-tt[idx],info,tt)
         
-
 def solution(n, info):
-
-    
-
     comp = [0,0,0,0,0,0,0,0,0,0,0]
     dfs(0,n,info,comp)
+    
+    if MAX_SCORE <= 0:
+        return [-1]
+    
+    MAX_LIST.sort(key= lambda x:x[::-1],reverse=True)
+    
+    return MAX_LIST[0]
 
-#info = [2,1,1,1,0,0,0,0,0,0,0]
-
-solution(5,[2,1,1,1,0,0,0,0,0,0,0])
+print(solution(5,[2,1,1,1,0,0,0,0,0,0,0]))
