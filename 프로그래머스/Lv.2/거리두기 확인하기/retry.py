b53b52933f5353ffd1f8e8572cc48fb4e4122b59
Leejoohyeong <cheng1230@nate.com>
2023-01-09 00:22:04 +0900

    거리두기 solution 리팩토링

--- 프로그래머스/Lv.2/거리두기 확인하기/retry.py
+++ 프로그래머스/Lv.2/거리두기 확인하기/retry.py
@@ -1,17 +1,12 @@
-import re
 from collections import deque
 
 def bfs(mat):
-
     user_pos = []
     
-
     for i in range(5):
         for j in range(5):
             if mat[i][j] == "P":
                 user_pos.append((i,j))
-    
-    
 
     dy = [0,0,-1,1]
     dx = [-1,1,0,0]
@@ -20,7 +15,6 @@ def bfs(mat):
         queue = deque([s])
         visited = [[0]*5 for _ in range(5)]
         distance = [[0]*5 for _ in range(5)]
-        visited[s[0]][s[1]] = 1
 
         while queue:
             y,x = queue.popleft()
@@ -31,12 +25,10 @@ def bfs(mat):
                 nx = x + dx[i]
                 
                 if 0<=ny<5 and 0<=nx<5 and visited[ny][nx]==0:
-                    if mat[ny][nx] == "O":
+                    if mat[ny][nx] == "O" and distance[y][x] ==0:
                         queue.append((ny,nx))
-                        visited[ny][nx] = 1
                         distance[ny][nx] = distance[y][x]+1
-                    
-                    elif mat[ny][nx] == "P" and distance[y][x] <=1:
+                    elif mat[ny][nx] == "P":
                         return 0    
     return 1
 
@@ -48,8 +40,3 @@ def solution(places):
     print(answer)
     return answer
 
-
-places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], 
-["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
-
-solution(places)
