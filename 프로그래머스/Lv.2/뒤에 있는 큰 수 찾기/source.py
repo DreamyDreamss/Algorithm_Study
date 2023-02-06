4d34f6d8803d3610c50467a2fb04ffafb9cdb574
Leejoohyeong <cheng1230@nate.com>
2023-02-06 23:54:47 +0900

    프로그래머스 lv2 업로드 23.2.6

--- 프로그래머스/Lv.2/뒤에 있는 큰 수 찾기/source.py
+++ 프로그래머스/Lv.2/뒤에 있는 큰 수 찾기/source.py
@@ -1,7 +1,31 @@
 def solution(numbers):
     answer = []
-
-    for i in numbers:
-        print(i)
-        
+    dic = dict()
+    
+    #for idx,num in enumerate(numbers):
+    #    dic[idx] = num
+    
+    #print(dic)
+    
+    len_num = len(numbers)
+    nxt_max_num = (0,-1)
+    
+    for idx,num in enumerate(numbers):
+        flag = 1
+        st = idx + 1
+        if num > nxt_max_num[1]:
+            st = nxt_max_num[0]+1
+            
+        for comp_idx in range(st,len_num):
+            #print(comp)
+            comp_num = numbers[comp_idx]
+            if num < comp_num:
+                answer.append(comp_num)
+                nxt_max_num = (comp_idx,comp_num)
+                flag = 0
+                break
+        if flag:
+            answer.append(-1)
+    
+    #print(answer)
     return answer
