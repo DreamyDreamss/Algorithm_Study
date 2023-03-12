289dd4ae575bc44b563897e05c20bb5cd0115137
Leejoohyeong <cheng1230@nate.com>
2023-03-12 22:17:04 +0900

    프로그래머스 lv2 업로드 (23.3.12)

--- 프로그래머스/Lv.2/미로 탈출/source.py
+++ 프로그래머스/Lv.2/미로 탈출/source.py
@@ -1,12 +1,36 @@
+from collections import deque 
+
+def bfs(pos):
+    deque = list()
+    deque.append(pos)
+    dx = [0,0,-1,1]
+    dy = [-1,1,0,0]
+
+    while deque:
+        ps = deque.pop(0)
+        for i in range(4):
+            x= ps[0]+dx[i]
+            y= ps[1]+dy[i]
+
+
+            
 def solution(maps):
     answer = 0
     st,lb = (),()
+    visited = [[] for _ in range(len(maps))]
     
-    for i in len(maps):
-        for j in len(maps[i]):
+    for i in range(len(maps)):
+        for j in range(len(maps[i])):
+            visited[i].append(0)
             if maps[i][j] == 'S':
                 st = (i,j)
             if maps[i][j] == 'L':
                 lb = (i,j)
-                
+    print(visited,st,lb)            
+    while deque:
+
+    
     return answer
+
+maps = ["SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"]
+solution(maps)
